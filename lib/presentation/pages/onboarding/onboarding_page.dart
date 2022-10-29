2345134da31900fd9126d66117b191d9d1d4a78e
lsalgado <ls140511@gmail.com>
2022-10-29 01:27:42 -0500

    Creation of Clean Architecture layers and main configuration

---
+++ lib/presentation/pages/onboarding/onboarding_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingPage extends StatelessWidget {
+  const OnBoardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('OnBoarding Screen'),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/pages/pages.dart
@@ -0,0 +1,4 @@
+export 'package:juicy_recipes/presentation/pages/onboarding/onboarding_page.dart';
+export 'package:juicy_recipes/presentation/pages/register/register_page.dart';
+export 'package:juicy_recipes/presentation/pages/login/login_page.dart';
+export 'package:juicy_recipes/presentation/pages/menu/menu_page.dart';
